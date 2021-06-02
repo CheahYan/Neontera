e56fa2dddba613f8f4490ecff2eb693ea40ed2d3
CheongYeeMing <e0526001@u.nus.edu>
2021-06-02 08:37:46 +0800

    Introduction/Tutorial Level
    Tilemap Edited.
    Parallax Background added.
    CameraController Script removed and replaced with Cinemachine.

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -34,7 +34,7 @@ public class ParallaxBackground : MonoBehaviour
         {
             if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
             {
-                float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+                float offsetPositionX =  (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
                 transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
             }
         }
